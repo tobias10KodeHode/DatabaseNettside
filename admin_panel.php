9e8f8ccbb57c3c0082f822ee6f367a2aed9d9b9f
tobias10kodehode <tobias10.kodehode@gmail.com>
2024-02-29 13:01:23 +0100

    Added displayed data to admin_panel.

--- admin_panel.php
+++ admin_panel.php
@@ -19,6 +19,26 @@
         </form>
     </div>
     <br>
+    <div class="remove_data">
+        <h1> User Info </h1>
+        <?php
+        // Include the database connection and query code
+        include 'remove_data.php';
+
+        // Check if there are any rows returned
+        if ($result->num_rows > 0) {
+            // Output data of each row
+            while ($row = $result->fetch_assoc()) {
+                // Display the data (you can format it as needed)
+                echo "ID: " . $row["id"] . " - Name: " . $row["fullname"] . " - Email: " . $row["email"] . " - Password: " . $row["cred"] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+
+
+        ?>
+    </div>
 </body>
 
 </html>
